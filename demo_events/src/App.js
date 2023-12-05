252fd512c7347cc1dfcc7a67816e2e917987b3fa
Aditya Pathak <sumitpathak861@gmail.com>
2023-12-05 13:48:06 +0530

    setState()

--- demo_events/src/App.js
+++ demo_events/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component
+{
+  constructor()
+  {
+    super();
+    this.state = { x:10 , name: "Aditya"}
+  }
+  changeData = ()=>{
+    this.setState({ x:100 , name: "Sumit"});
+  }
+  render()
+  {
+    return (
+      <>
+        <h1 style={{ textAlign: "center", color: "red" }}> Events </h1>
+        <b>Value of x is: {this.state.x}</b> &nbsp;&nbsp;
+        <b>Name is: {this.state.name}</b> <br />
+        <br />
+        <button onClick={this.changeData}> Change Data</button> &nbsp;&nbsp;
+        <button onClick={() => alert("Hello...")}> Click Here</button>{" "}
+        &nbsp;&nbsp;
+        <button onClick={()=> this.setState({x: 200, name :"Vilamidir"})}> Click Here</button>
+      </>
+    );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
 }
-
-export default App;
+export default App
