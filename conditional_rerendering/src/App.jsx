9d281b9a37b70555623ced9dcca036a17b9cee95
Aditya Pathak <sumitpathak861@gmail.com>
2023-12-15 15:15:47 +0530

    😎

--- conditional_rerendering/src/App.jsx
+++ conditional_rerendering/src/App.jsx
@@ -4,10 +4,12 @@ import User from "./components/User";
 const App = (props) =>
 { 
   const result = props.data
-    return<div>
-      <h1>Main Component Called</h1>
-      {(result)? <User/> : <Show/>}
-    </div>
+    return (
+      <div>
+        <h1>Main Component Called</h1>
+        {result ? <User /> + "👌" : <Show />}
+      </div>
+    );
 
 }
 export default App;
